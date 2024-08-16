8f9be2eabf5cb97a7c5e03d188e626956017cfac
ferreirar8 <139363746+ferreirar8@users.noreply.github.com>
2024-08-16 21:38:16 +0100

    624

--- MaximumDistanceInArrays.cpp
+++ MaximumDistanceInArrays.cpp
@@ -1,15 +1,15 @@
 class Solution {
 public:
     int maxDistance(vector<vector<int>>& arrays) {
-        int min_so_far = arrays[0].front();  // Initialize to the first element of the first array
-        int max_so_far = arrays[0].back();   // Initialize to the last element of the first array
+        int min_so_far = arrays[0].front();
+        int max_so_far = arrays[0].back();
         int max_distance = 0;
         
         for (int i = 1; i < arrays.size(); i++) {
-            int min_current = arrays[i].front();  // First element of the current array
-            int max_current = arrays[i].back();   // Last element of the current array
+            int min_current = arrays[i].front();
+            int max_current = arrays[i].back();
             
-            // Calculate the distance with the min and max from previous arrays
+            // Calculate max distance using elements from different arrays
             max_distance = max(max_distance, max(abs(max_current - min_so_far), abs(max_so_far - min_current)));
             
             // Update min_so_far and max_so_far
