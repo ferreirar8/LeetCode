49486ab040ced8d309505d209dccc52e51df34f6
ferreirar8 <139363746+ferreirar8@users.noreply.github.com>
2024-08-16 11:49:17 +0100

    Update LemonadeChange.cpp

--- LemonadeChange.cpp
+++ LemonadeChange.cpp
@@ -1,27 +1,27 @@
 class Solution {
 public:
     bool lemonadeChange(vector<int>& bills) {
-    int n = bills.size();
-    int total = 0;
-    int count_5 = 0;
-    int count_10 = 0;
-    for (int i = 0; i < n; i++) {
-        if (bills[i] == 5) {
-            count_5 ++;
-        }else if (bills[i] == 10) {
-            count_10 ++ ;
-            count_5 --;
-        }else{
-            if (count_10 > 0) {
-                count_10 --;
-                count_5 --;
+        int count_5 = 0;
+        int count_10 = 0;
+
+        for (int bill : bills) {
+            if (bill == 5) {
+                count_5++;
+            } else if (bill == 10) {
+                if (count_5 == 0) return false;
+                count_5--;
+                count_10++;
             } else {
-                count_5 -=3;
+                if (count_10 > 0 && count_5 > 0) {
+                    count_10--;
+                    count_5--;
+                } else if (count_5 >= 3) {
+                    count_5 -= 3;
+                } else {
+                    return false;
+                }
             }
         }
-        if (count_5 < 0 || count_10 < 0)
-            return false;
-        }
-    return true;
+        return true;
     }
 };
